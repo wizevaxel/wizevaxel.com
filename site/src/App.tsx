3677213c96c9fb4bb96d2d7ac9a901cabba51138
wizevaxel <84578287+wizevaxel@users.noreply.github.com>
2026-08-25 06:21:17 +0300

    setup basic page

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -1,7 +1,19 @@
 function App() {
   return (
     <>
-      <h1>wizeväxel</h1>
+      <div class="container">
+        <div class="card">
+          <h1>wizeväxel</h1>
+          <h3>expression thru media</h3>
+        </div>
+        <div class="card">
+          <ul>
+            <li><a href="https://youtube.com/@wizevaxel">YouTube</a></li>
+            <li><a href="https://github.com/wizevaxel">GitHub</a></li>
+            <li><a href="https://open.spotify.com/artist/3b3SM98HFVmOcE46DSgGwQ">Spotify (LiterallyWize)</a></li>
+          </ul>
+        </div>
+      </div>
     </>
   )
 }
