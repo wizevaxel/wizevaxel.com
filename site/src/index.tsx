94512ddc7be42c561e098c26178b482e809ddcfa
wizevaxel <84578287+wizevaxel@users.noreply.github.com>
2026-08-25 06:20:25 +0300

    add inter font

--- site/src/index.tsx
+++ site/src/index.tsx
@@ -1,6 +1,7 @@
 /* @refresh reload */
 import { render } from 'solid-js/web'
 import './index.css'
+import '@fontsource/inter/500.css'
 import App from './App.tsx'
 
 const root = document.getElementById('root')
