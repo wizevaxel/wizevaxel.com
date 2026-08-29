a271c4dad764c13264b98f2c2a85f76621fc1c2e
wizevaxel <84578287+wizevaxel@users.noreply.github.com>
2026-08-29 10:41:51 +0300

    add more links and contact email

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -10,7 +10,10 @@ function App() {
         <Card>
           <div style="padding: 0.4em" class="center-content">
             <SvgBrandText width="24em" height="auto"/>
-            <h3 class="brand">expression thru media</h3>
+            <h2 class="brand">expression thru media</h2>
+            <h4 style="padding-top: 0.6em">
+              contact: <a href="mailto:hello@wizevaxel.com">hello@wizevaxel.com</a>
+            </h4>
           </div>
         </Card>
         <Card title="Links">
@@ -18,6 +21,8 @@ function App() {
           <LinkButton href="https://github.com/wizevaxel" text="GitHub" icon="github"/>
           <LinkButton href="https://wizevaxel.itch.io/" text="itch.io" icon="itchdotio"/>
           <LinkButton href="https://roblox.com/users/1341839736/profile" text="Roblox" icon="roblox"/>
+          <LinkButton href="https://x.com/wizevaxel" text="X" icon="x"/>
+          <LinkButton href="https://bsky.app/profile/wizevaxel.bsky.social" text="Bluesky" icon="bluesky"/>
           <LinkButton href="https://music.youtube.com/@wizevaxel" text="YouTube Music" icon="youtubemusic"/>
           <LinkButton href="https://open.spotify.com/artist/3b3SM98HFVmOcE46DSgGwQ" text="(LiterallyWize) Spotify" icon="spotify"/>
           <LinkButton href="https://music.apple.com/artist/1597124587" text="(LiterallyWize) Apple Music" icon="applemusic"/>
